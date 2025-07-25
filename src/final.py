dc3c177fee5a693a49cec4ec8e686fa5e9c47fb2
sibsd123 <sibsd@terpmail.umd.edu>
2025-07-25 08:44:36 -0400

    its working - anakin skywalker

--- src/final.py
+++ src/final.py
@@ -3,6 +3,9 @@ import numpy as np
 import remodnav as rm
 import os 
 import shutil as su
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def prep_remodnav_df(raw: pd.DataFrame, height: int, width: int, debug1: str = None, debug2: str = None, outfile: str = None):
     # height: 2740 px
@@ -98,16 +101,17 @@ def remodnav_feature_extract(filenames):
 
             df_dats.loc[idx, 'μ_t_' + type] = np.mean(df[df['label'].isin(labels)]['duration'])
             df_dats.loc[idx, 'σ_t_' + type] = np.std(df[df['label'].isin(labels)]['duration'])
-            df_dats.loc[idx, 'μ_v_max_' + type] = np.mean(df[df['label'].isin(labels)]['peak_vel'])
-            df_dats.loc[idx, 'σ_v_max_' + type] = np.std(df[df['label'].isin(labels)]['peak_vel'])
+            df_dats.loc[idx, 'μ_vmax_' + type] = np.mean(df[df['label'].isin(labels)]['peak_vel'])
+            df_dats.loc[idx, 'σ_vmax_' + type] = np.std(df[df['label'].isin(labels)]['peak_vel'])
             df_dats.loc[idx, 'μ_v_' + type] = np.average(a = df[df['label'].isin(labels)]['avg_vel'], weights = df[df['label'].isin(labels)]['duration'])
     
     df_dats.index = filenames
+    return df_dats
 
 def get_trials_from_mode(subject_path: str, mode_csv: str):
     timestamps_path = os.path.join(subject_path, 'processed_timestamps', mode_csv)
     data_path = os.path.join(subject_path, 'Varjo', mode_csv)
-    
+
     timestamps = pd.read_csv(timestamps_path, header = None)
     df = pd.read_csv(data_path)
 
@@ -131,7 +135,19 @@ def get_baseline(subject_path: str):
 
     return baseline
 
-def process_all_data(data_path: str, workspace: str, height: int = 2740, width: int = 2880, PPD: int = 35, savgol_length: float = 0.023, filter_length: float = 0.005):
+def get_pupillometry(data: pd.DataFrame):
+    df = pd.DataFrame({'average_openness' : [0.0], 'average_pupil_diameter' : [0.0]})
+    left_average_openness = data['left_eye_openness'].mean(skipna = True)
+    right_average_openness = data['right_eye_openness'].mean(skipna = True)
+    left_average_diameter = data['left_pupil_diameter_in_mm'].mean(skipna = True)
+    right_average_diameter = data['right_pupil_diameter_in_mm'].mean(skipna = True)
+
+    df.loc[0, 'average_openness'] = (left_average_openness + right_average_openness) / 2.0
+    df.loc[0, 'average_pupil_diameter'] = (left_average_diameter + right_average_diameter) / 2.0
+    return df
+
+
+def process_all_data(data_path: str, workspace: str, workload_path: str, height: int = 2740, width: int = 2880, PPD: int = 35, savgol_length: float = 0.023, filter_length: float = 0.005, bedford_encoding_threshold: int = 6):
     subject_names = []
     modes = ['DVE', 'DVEH', 'GVE', 'GVEH']
 
@@ -145,9 +161,12 @@ def process_all_data(data_path: str, workspace: str, height: int = 2740, width: 
         os.mkdir(workspace)
 
     data = []
+    wrkld = pd.read_excel(workload_path)
+    wrkld.index = wrkld['Subject']
+    wrkld.drop('Subject', axis = 1, inplace = True)
 
     for subject_name in subject_names:
-        subject_path = os.path.join(data_path, subdir)
+        subject_path = os.path.join(data_path, subject_name)
 
         subject_data = []
         baseline = get_baseline(subject_path)
@@ -161,14 +180,119 @@ def process_all_data(data_path: str, workspace: str, height: int = 2740, width: 
 
         data.append(subject_data)
 
-    print(data)
-
     for i, subject_data in enumerate(data):
         for j, mode_data in enumerate(subject_data):
             if j == 0:
                 full_run_remodnav(mode_data, os.path.join(workspace, subject_names[i] + '_baseline_rmexp.csv'), height, width, PPD, savgol_length, filter_length)
+                df = get_pupillometry(mode_data)
+                df['bedford_workload'] = [0]
+                df['bedford_encoding'] = [0]
+                df.to_csv(os.path.join(workspace, subject_names[i] + '_baseline_exexp.csv'), index = False)
                 continue
             else:
                 for k, trial_data in enumerate(mode_data):
                     full_run_remodnav(trial_data, os.path.join(workspace, subject_names[i] + '_' + modes[j - 1] + '_' + str(k) + '_rmexp.csv'), height, width, PPD, savgol_length, filter_length)
-                
+                    df = get_pupillometry(trial_data)
+                    df.loc[0, 'bedford_workload'] = wrkld.loc[subject_names[i], modes[j - 1]]
+                    df['bedford_encoding'] = df['bedford_workload'].apply(lambda x: 1 if x < bedford_encoding_threshold else 2)
+                    df.to_csv(os.path.join(workspace, subject_names[i] + '_' + modes[j - 1] + '_' + str(k) + '_exexp.csv'), index = False)
+
+def load_export(workspace: str):
+    allfiles = os.listdir(workspace)
+
+    rmfilenames = []
+    exfilenames = []
+
+    for file in allfiles:
+        if file.endswith('rmexp.csv'):
+            rmfilenames.append(os.path.join(workspace, file))
+        elif file.endswith('.csv'):
+            exfilenames.append(os.path.join(workspace, file))
+        else:
+            os.remove(os.path.join(workspace, file))
+    
+    df = remodnav_feature_extract(rmfilenames)
+
+    df['μ_openness'] = 0.0
+    df['μ_diameter'] = 0.0
+    df['bedford'] = 0
+    df['bedford_enc'] = 0
+
+    for file in exfilenames:
+        indexpath = file.replace('exexp', 'rmexp')
+        ex = pd.read_csv(file)
+        ex.columns = ['μ_openness', 'μ_diameter', 'bedford', 'bedford_enc']
+        for col in ['μ_openness', 'μ_diameter', 'bedford', 'bedford_enc']:
+            df.loc[indexpath, col] = ex.loc[0, col]
+    df['Subject'] = df.index
+    df['Mode'] = df['Subject'].apply(lambda x: re.search(r'_(baseline|DVEH|DVE|GVEH|GVE)', x).group(1))
+    df['Subject'] = df['Subject'].apply(lambda x: re.search(r'Subject_(\d+)', x).group(1))
+    df.reset_index(inplace = True, drop = True)
+    return df
+
+def baseline_normalize(df: pd.DataFrame):
+
+    baseline = df[df['Mode'] == 'baseline'].set_index('Subject', drop = True)
+    exclude_cols = ['bedford', 'bedford_enc', 'Subject', 'Mode']
+    include_cols = df.columns.difference(exclude_cols)
+    
+    normalized_values = df.apply(lambda row: (row[include_cols] - baseline.loc[row["Subject"], include_cols]) / baseline.loc[row["Subject"], include_cols], axis=1)
+    df_normalized = pd.concat([df[exclude_cols], normalized_values], axis=1)
+
+    return df_normalized[df_normalized['Mode'] != 'baseline']
+    
+def average_trials(df: pd.DataFrame):
+    df_averaged = df.groupby(['Subject', 'Mode']).mean().reset_index()
+
+    return df_averaged
+
+def z_score_standardize(df: pd.DataFrame):
+    include_cols = df.columns.difference(['Subject', 'Mode', 'bedford', 'bedford_enc'])
+
+    df_zscored = df.copy()
+    df_zscored[include_cols] = df.groupby('Mode')[include_cols].transform(
+        lambda x: (x - x.mean() / x.std(ddof = 0))
+    )
+
+    return df_zscored
+
+
+def analyze_all_data(workspace: str, func1 = lambda x: x, func2 = lambda x: x, func3 = lambda x: x):
+    df = load_export(workspace)
+
+    df = func1(df)
+
+    df = func2(df)
+
+    df = func3(df)
+
+    print(df)
+    return df
+
+def plotter(df: pd.DataFrame):
+    sns.set(style = 'whitegrid')
+
+    include_cols = df.columns.difference(['Subject', 'Mode', 'bedford', 'bedford_enc'])
+
+    n_cols = 4
+    n_rows = -(-len(include_cols) // n_cols)  # ceiling division
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(5*n_cols, 4*n_rows), constrained_layout=True)
+    axes = axes.flatten()
+
+    for i, col in enumerate(include_cols):
+        sns.boxplot(data=df, x='bedford_enc', y=col, ax=axes[i])
+        axes[i].set_title(f'Boxplot of {col} by bedford_enc')
+
+    for j in range(i+1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plot_path = "out.png"
+    plt.savefig(plot_path, dpi=300)
+    plt.close()
+
+    plot_path
+
+
+df = analyze_all_data('wrk', baseline_normalize, z_score_standardize)
+print(df['bedford_enc'].value_counts())
+plotter(df)
